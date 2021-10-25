2cec1b6568833b34a9b2ca227144b15a0b5d6c56
Igor812 <92649492+Igor812@users.noreply.github.com>
2021-10-25 23:22:43 +0300

    задание 4

--- hw1/bin/stringcount.dart
+++ hw1/bin/stringcount.dart
@@ -1,14 +1,31 @@
 
+import 'dart:collection';
+
 class StringCount{
 
   Map<String,int> countElement (String inputString){
 
-    Map<String,int> resultStr ={"1":1};
-    List<String> elementsString = inputString.split(" ");
+    Map<String,int> resultStr ={};
+    RegExp exp = RegExp(r"[^a-zA-Zа-яА-Я]+");
+    List<String> elementsString = inputString.replaceAll(exp," ").split(" ");
+    elementsString = elementsString.map((email)=>email.toLowerCase()).toList();
+    print(elementsString);
+    Set<String> subBuffer = {};
 
     for(int i = 0 ; i<elementsString.length;i++){
-      print(elementsString);
+      subBuffer.add(elementsString[i]);
     }
+
+      for(int j = 0; j<subBuffer.length;j++) {
+      int count = 0;
+      for (int i=0; i<elementsString.length; i++) {
+        if (elementsString[i]==subBuffer.elementAt(j)){
+          count=count+1;
+        }
+        resultStr[subBuffer.elementAt(j)]=count;
+      }
+    }
+    print(resultStr.toString());
     return resultStr;
     }
 
